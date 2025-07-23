9ec487203b043aab9d737f9398ea0c002b3b3fab
gabriele <gabriele1413@gmail.com>
2025-07-23 13:35:39 +0200

    change to postgre

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-/* const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
     dialectOptions: {
         ssl: {
@@ -8,12 +8,12 @@ const { Sequelize } = require("sequelize");
             rejectUnauthorized: false,
         },
     },
-}); */
+});
 
-const sequelize = new Sequelize({
+/* const sequelize = new Sequelize({
     dialect: "sqlite",
     storage: "database/db.sqlite",
     logging: console.log
-})
+}) */
 
 module.exports = sequelize;
