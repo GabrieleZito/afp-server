dda8dad01ab3127579d29d50060d3044adeac759
gabriele <gabriele1413@gmail.com>
2025-07-23 12:13:35 +0200

    change sequelize config

--- config/sequelize.js
+++ config/sequelize.js
@@ -1,6 +1,6 @@
 const { Sequelize } = require("sequelize");
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+/* const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: "postgres",
     dialectOptions: {
         ssl: {
@@ -8,6 +8,12 @@ const sequelize = new Sequelize(process.env.DATABASE_URL, {
             rejectUnauthorized: false,
         },
     },
-});
+}); */
+
+const sequelize = new Sequelize({
+    dialect: "sqlite",
+    storage: "database/db.sqlite",
+    logging: console.log
+})
 
 module.exports = sequelize;
